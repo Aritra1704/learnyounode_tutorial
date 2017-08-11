a88571df1956656f2842ad86e2708d6d88655e28
aritrapal <aritra.pal@zippr.in>
2017-08-11 21:31:49 +0530

    Read file completed

---
+++ program4.js
@@ -0,0 +1,24 @@
+var fs = require('fs')
+    var file = process.argv[2]
+
+    var callback = function (err, contents) {
+      if (err) {
+        return console.log(err)
+      }
+      // fs.readFile(file, 'utf8', callback) can also be used
+      var lines = contents.toString().split('\n').length - 1
+      console.log(lines)
+    }
+
+    fs.readFile(file, callback)
+
+
+
+    // fs.readFile(file, function (err, contents) {
+    //   if (err) {
+    //     return console.log(err)
+    //   }
+    //   // fs.readFile(file, 'utf8', callback) can also be used
+    //   var lines = contents.toString().split('\n').length - 1
+    //   console.log(lines)
+    // })
